b2ee4f37fa3c149452fb2abb1e020848e4f94948
Shajith Chacko <shajith.chacko@gmail.com>
2012-11-03 18:19:51 -0700

    fmt, Assignee

--- lotus.go
+++ lotus.go
@@ -8,41 +8,59 @@ import "errors"
 
 type Client struct {
 	Subdomain string
-  User string
-	Password string
+	User      string
+	Password  string
 }
 
 type User struct {
-	Url string
-	Id int
-  Name string
-	Email string
-	Role string
+	Url               string
+	Id                int
+	Name              string
+	Email             string
+	Role              string
 	AuthenticityToken string `json:"authenticity_token"`
-	Locale string
-	Tags []string
+	Locale            string
+	Tags              []string
 }
 
 type View struct {
-	Url string
-	Id int
-	Title string
+	Url    string
+	Id     int
+	Title  string
 	Active bool
 }
 
 type Ticket struct {
-	Url string
-	Id int
-	Subject string
+	Url         string
+	Id          int
+	Subject     string
 	Description string
-	Tags []string
+	Tags        []string
+	AssigneeId  int `json:"assignee_id"`
+	RequesterId int `json:"requester_id"`
+	requester   *User
+	assignee    *User
+}
+
+func (c *Client) Assignee(t Ticket) (user *User, err error) {
+	if t.assignee != nil {
+		user = t.assignee
+	} else {
+		if t.AssigneeId != 0 {
+			user, err = c.getUser(fmt.Sprintf("%d", t.AssigneeId))
+		} else {
+			user, err = nil, nil
+		}
+	}
+
+	return
 }
 
 type ViewResult struct {
-  NextPageUrl string `json:"next_page"`
-  PreviousPageUrl string `json:"previous_page"`
-	Count int
-  Tickets []Ticket
+	NextPageUrl     string `json:"next_page"`
+	PreviousPageUrl string `json:"previous_page"`
+	Count           int
+	Tickets         []Ticket
 }
 
 func (c *Client) ExecuteViewNextPage(r *ViewResult) (result *ViewResult, err error) {
@@ -56,31 +74,41 @@ func (c *Client) ExecuteViewNextPage(r *ViewResult) (result *ViewResult, err err
 }
 
 func (c *Client) ExecuteView(v View) (*ViewResult, error) {
-	var url = fmt.Sprintf("/views/%d/tickets.json", v.Id);
-	return c.getViewResults(url);
+	var url = fmt.Sprintf("/views/%d/tickets.json", v.Id)
+	return c.getViewResults(url)
 }
 
-func (c *Client) getViewResults(path string) (data *ViewResult, err error) {
-	body, err := c.perform("GET", path)
-	json.Unmarshal(body, &data)
-	return data, err;
-}
-
-func (c *Client) Me() User {
-	var data = make(map[string]User)
-	var body, _ = c.perform("GET", "/users/me.json")
-	json.Unmarshal(body, &data)
-  return data["user"]
+func (c *Client) Me() (*User, error) {
+	return c.getUser("me")
 }
 
 func (c *Client) Views() []View {
 	var data = make(map[string][]View)
 	var body, _ = c.perform("GET", "/views.json")
 	json.Unmarshal(body, &data)
-  return data["views"]
+	return data["views"]
 }
 
-func (c *Client) perform(method string, path string) (body []byte, err error){
+func (c *Client) getViewResults(url string) (data *ViewResult, err error) {
+	body, err := c.perform("GET", url)
+	json.Unmarshal(body, &data)
+	return data, err
+}
+
+func (c *Client) getUser(id string) (user *User, err error) {
+	var data = make(map[string]User)
+	body, err := c.perform("GET", "/users/me.json")
+
+	if err == nil {
+		json.Unmarshal(body, &data)
+		u := data["user"]
+		user = &u
+	}
+
+	return
+}
+
+func (c *Client) perform(method string, path string) (body []byte, err error) {
 	var httpClient http.Client
 	var url = fmt.Sprintf("https://%s.zendesk.com/api/v2%s", c.Subdomain, path)
 	req, err := http.NewRequest(method, url, nil)
